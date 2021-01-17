2d19325e0085ab11719f1f79bb1a0b9554ad0f2c
Mushvig <guluzadamushvig@gmail.com>
2021-01-17 23:14:29 +0400

    Added ClassMethodsDemo

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer added successfully:" + customer.Name);
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer deleted successfully:" + customer.CustomerId);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+
+            customer.Name = "Mushvig";
+            customer.Sname = "Guluzada";
+            customer.CustomerId = 1211111;
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.AddCustomer(customer);
+            customerManager.DeleteCustomer(customer);
+        }
+    }
+}
