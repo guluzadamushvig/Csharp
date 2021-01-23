b85cdc9abef41a0e2418f250b63a28d36995012d
Mushvig <guluzadamushvig@gmail.com>
2021-01-23 14:46:53 +0400

    Constructors

---
+++ Constructors/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+
+        public Customer(int id, string name, string sname)
+        {
+            Id = id;
+            Name = name;
+            SName = sname;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SName { get; set; }
+
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+
+            ////first use
+
+            customer.Id = 1;
+            customer.Name = "Mushvig";
+            customer.SName = "Guluzada";
+
+            ////second use
+
+            Customer customer2 = new Customer() { Id = 2, Name = "Nigar", SName = "Guluzada" };
+
+
+            //// third use
+
+            Customer customer3 = new Customer(3,"Arslan","Guluzada");
+        }
+    }
+}
