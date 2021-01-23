e2f1ecae044be3b0c4d4ec09af162fca15111b61
Mushvig <guluzadamushvig@gmail.com>
2021-01-23 15:28:30 +0400

    MyDictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> data = new MyDictionary<int, string>();
+            data.Add(1, "Mushvig");
+            data.Add(589, "Guluzada");
+
+            data.GetData();
+
+        }
+    }
+}
